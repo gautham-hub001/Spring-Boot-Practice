14eb7a6bfdbc693f817f73d0836b6d8cc0b04371
gautham-hub001 <gautham2pothana@gmail.com>
2023-03-08 15:29:44 -0500

    jasper error fix

--- my-demo/src/main/java/com/example/mydemo/HomeController.java
+++ my-demo/src/main/java/com/example/mydemo/HomeController.java
@@ -1,15 +1,25 @@
 package com.example.mydemo;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
     @RequestMapping("home")
-    @ResponseBody
-    public String home() {
+    public String home(HttpServletRequest req) {
+        String name = req.getParameter("name");
+
+        HttpSession session = req.getSession();
+        session.setAttribute("name", name);
+
+        // System.out.println(name);
+        // req.setAttribute("name", name);
+
         System.out.println("/home called");
-        return "home.jsp";
+        System.out.println(name);
+        return "home";
     }
 }
---
+++ my-demo/src/main/webapp/pages/home.jsp
@@ -0,0 +1,14 @@
+<!-- <%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" %>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN” “http: //www.w3.org/TR/html14/Loose.dtd"> -->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <title>Home page</title>
+</head>
+<body>
+    <h1>I love java</h1>
+    <h1>${name}</h1>
+</body>
+</html>
